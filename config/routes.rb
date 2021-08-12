da8549fff4e67a679252b58a9cc23d457f55df1a
amu1016 <amu.prog@gmail.com>
2021-08-12 12:24:26 +0900

    プロフィール編集画面ビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: "clubs#index"
   resources :clubs, only: [:index]
   resources :users, only: [:show]
