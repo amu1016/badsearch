b279658f92c020db3a1516e9f93f83f97d041670
amu1016 <amu.prog@gmail.com>
2021-08-15 23:21:57 +0900

    クラブ新規作成ビューの整理

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,12 +1,10 @@
 class ClubsController < ApplicationController
   before_action :authenticate_user!, except: :index
-  before_action :move_to_index, 
 
 
 
 
   def index
-    @clubs = Item.includes(:club).order("created_at DESC")
   end
 
   def new
@@ -28,7 +26,4 @@ class ClubsController < ApplicationController
     params.require(:club).permit(:image, :name, :status_id, :since_year, :since_month, :prefecture_id, :city, :gym, :gender_ratio, :beginner_ratio, :age_range, :purpose, :homepage, :information).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to action: :index if current_user.id != @club.user.id
-  end
 end
