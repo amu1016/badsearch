62be3a08b2b44e3a2e219196508740cbd5ec4aee
amu1016 <amu.prog@gmail.com>
2021-08-15 23:53:54 +0900

    clubモデル バリデーション設定

--- app/models/club.rb
+++ app/models/club.rb
@@ -5,4 +5,19 @@ class Club < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :prefecture
 
+
+  with_options presence: true do
+    validates :name
+    validates :status_id
+    validates :since_year
+    validates :since_month
+    validates :prefecture_id
+    validates :city
+    validates :gym
+    validates :gender_ratio
+    validates :beginner_ratio
+    validates :age_range
+    validates :purpose
+    validates :information
+  end
 end
