9c689870d969137d56affe16ede9ba2f9973aba9
amu1016 <amu.prog@gmail.com>
2021-08-16 17:12:39 +0900

    ユーザー詳細ページにおける分岐設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :clubs
+  has_one :club
 
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :prefecture
