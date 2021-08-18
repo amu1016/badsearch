a38978453843319959d7bbb6db56010e5178e6d3
amu1016 <amu.prog@gmail.com>
2021-08-18 17:45:45 +0900

    Chat開始確認画面のビュー作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,9 +4,24 @@ class RoomsController < ApplicationController
   end
 
   def new
+    @room = Room.new
+    @club = Club.find(params[:club_id])
   end
 
   def create
+    @club = Club.find(params[:club_id])
+    @room = Room.new(name: @club.name, user_ids:[current_user.id, @club.user_id])
+    if @room.save
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def room_params
+    params.require(:club).permit(:user_id)
   end
 
 end
