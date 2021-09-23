7cc24510c62557c3f94828f3873b9061ef688175
amu1016 <amu.prog@gmail.com>
2021-09-23 12:14:43 +0900

    modalの設置

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require('../clubs/preview')
 require('../clubs/slider')
 require('../clubs/search_accordion')
 require('../clubs/city_accordion')
+require('../clubs/modal')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
