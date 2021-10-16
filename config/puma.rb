6b08b898bc5229fe4110533f69a9f4fcdcbc7b38
amu1016 <amu.prog@gmail.com>
2021-10-16 23:24:36 +0900

    puma.rb,nginx.conf 修正

--- config/puma.rb
+++ config/puma.rb
@@ -39,6 +39,7 @@ plugin :tmp_restart
 
 
 app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+# bind "unix://#{app_root}/tmp/sockets/puma.sock"
+bind "unix:///var/www/rails/#{app_root}/tmp/sockets/puma.sock" #本番
 
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
