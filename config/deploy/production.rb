ee49035fcd6dd327ca8cd21214e1b8fd84502dc3
amu1016 <amu.prog@gmail.com>
2021-11-09 03:12:29 +0900

    server name修正②

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server 'bad-search.com', user: 'ec2-user', roles: %w{app db web}
+server '35.72.59.220', user: 'ec2-user', roles: %w{app db web}
