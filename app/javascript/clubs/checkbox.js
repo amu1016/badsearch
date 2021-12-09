980e195263635075d9064bab0985c737dea766ee
amu1016 <amu.prog@gmail.com>
2021-12-09 23:41:12 +0900

    Update checkbox.js

--- app/javascript/clubs/checkbox.js
+++ app/javascript/clubs/checkbox.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
-  if( document.getElementById('club_search')){
-    var queryString = window.location.search;
-    var queryObject = new Object();
-    if(queryString){
-      queryString = queryString.substring(1);
-      var replacedQueryString = queryString.replace("q%5B", "").replace("%5D", "").replace("%5D%5B%5D", "").replace("%E4%BB%A3", "").replace("", "")
-      var parameters = replacedQueryString.split('&');
-      for (var i = 0; i < parameters.length; i++) {
-        var elementId = parameters[i].replace("=", "_");
+  var queryString = window.location.search;
+  var queryObject = new Object();
+  if(queryString){
+    queryString = queryString.substring(1);
+    var replacedQueryString = queryString.replace("q%5B", "").replace("%5D", "").replace("%5D%5B%5D", "").replace("%E4%BB%A3", "").replace("", "")
+    var parameters = replacedQueryString.split('&');
+    for (var i = 0; i < parameters.length; i++) {
+      var elementId = parameters[i].replace("=", "_");
+      if(document.getElementById(elementId)){
         var checkTargets = document.getElementById(elementId)
         checkTargets.checked = true;
       };
-
-      
     };
   };
 });
