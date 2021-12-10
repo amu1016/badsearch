e3f2738ec364ffcb7fa87d29231a37ccd1144ca0
amu1016 <amu.prog@gmail.com>
2021-12-10 16:57:13 +0900

    Update checkbox.js

--- app/javascript/clubs/checkbox.js
+++ app/javascript/clubs/checkbox.js
@@ -1,37 +1,37 @@
-window.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
   var queryString = window.location.search;
   var queryObject = new Object();
   if(queryString){
     queryString = queryString.substring(1);
-    var parameters = queryString.split('&');
-    var decodeParameters = decodeURIComponent(parameters);
-    for (var i = 0; i < decodeParameters.length; i++) {
-      var element = decodeParameters[i].split('=');
+    var decodeParameters = decodeURIComponent(queryString);
+    var parameters = decodeParameters.split('&');
+    for (var i = 0; i < parameters.length; i++) {
+      var element = parameters[i].split('=');
       var paramName = element[0];
       var paramValue = element[1];
       queryObject[paramName] = paramValue;
 
       // チェックボックス保持
       if (paramName === "q[status_id_eq_any][]") {
-        var elementId = decodeParameters[i].replace("[", "");
+        var elementId = parameters[i].replace("[", "_");
         elementId = elementId.replace("][]=", "_");
         var checkTargets = document.getElementById(elementId);
         checkTargets.checked = true;
       };
       if (paramName === "q[want_sex_id_eq_any][]") {
-        var elementId = decodeParameters[i].replace("=", "_");
+        var elementId = parameters[i].replace("[", "_");
         elementId = elementId.replace("][]=", "_");
         var checkTargets = document.getElementById(elementId);
         checkTargets.checked = true;
       };
       if (paramName === "q[want_ages_name_eq_any][]") {
-        var elementId = decodeParameters[i].replace("=", "_");
+        var elementId = parameters[i].replace("[", "_");
         elementId = elementId.replace("][]=", "_");
         var checkTargets = document.getElementById(elementId);
         checkTargets.checked = true;
       };
       if (paramName === "q[want_levels_name_eq_any][]") {
-        var elementId = decodeParameters[i].replace("=", "_");
+        var elementId = parameters[i].replace("[", "_");
         elementId = elementId.replace("][]=", "_");
         var checkTargets = document.getElementById(elementId);
         checkTargets.checked = true;
