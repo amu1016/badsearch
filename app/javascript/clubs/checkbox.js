49b442f55be966c5aa2315994b28d2913d3dc9e8
amu1016 <amu.prog@gmail.com>
2021-12-10 15:44:37 +0900

    チェックボックス保持

--- app/javascript/clubs/checkbox.js
+++ app/javascript/clubs/checkbox.js
@@ -3,12 +3,37 @@ document.addEventListener('DOMContentLoaded', function(){
   var queryObject = new Object();
   if(queryString){
     queryString = queryString.substring(1);
-    var replacedQueryString = queryString.replace("q%5B", "").replace("%5D", "").replace("%5D%5B%5D", "").replace("%E4%BB%A3", "")
-    var parameters = replacedQueryString.split('&');
-    for (var i = 0; i < parameters.length; i++) {
-      var elementId = parameters[i].replace("=", "_");
-      if(document.getElementById(elementId)){
-        var checkTargets = document.getElementById(elementId)
+    var parameters = queryString.split('&');
+    var decodeParameters = decodeURIComponent(parameters);
+    for (var i = 0; i < decodeParameters.length; i++) {
+      var element = decodeParameters[i].split('=');
+      var paramName = element[0];
+      var paramValue = element[1];
+      queryObject[paramName] = paramValue;
+
+      // チェックボックス保持
+      if (paramName === "q[status_id_eq_any][]") {
+        var elementId = decodeParameters[i].replace("[", "");
+        elementId = elementId.replace("][]=", "_");
+        var checkTargets = document.getElementById(elementId);
+        checkTargets.checked = true;
+      };
+      if (paramName === "q[want_sex_id_eq_any][]") {
+        var elementId = decodeParameters[i].replace("=", "_");
+        elementId = elementId.replace("][]=", "_");
+        var checkTargets = document.getElementById(elementId);
+        checkTargets.checked = true;
+      };
+      if (paramName === "q[want_ages_name_eq_any][]") {
+        var elementId = decodeParameters[i].replace("=", "_");
+        elementId = elementId.replace("][]=", "_");
+        var checkTargets = document.getElementById(elementId);
+        checkTargets.checked = true;
+      };
+      if (paramName === "q[want_levels_name_eq_any][]") {
+        var elementId = decodeParameters[i].replace("=", "_");
+        elementId = elementId.replace("][]=", "_");
+        var checkTargets = document.getElementById(elementId);
         checkTargets.checked = true;
       };
     };
