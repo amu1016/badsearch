46e2ba6f9ffcd42b8e257cc729c1da8a4a9aab16
amu1016 <amu.prog@gmail.com>
2021-12-12 02:58:55 +0900

    検証

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -15,7 +15,7 @@ if( document.getElementsByClassName('prefecture-city')){
     new Vue({
       el: '.prefecture-city',
       data: {
-        selectedPrefecture: ''`${prefectureId}`,
+        selectedPrefecture: '10',
         cities: [], 
         prefectures: JSON.parse(gon.prefectures)
       },
