26302cf01242e2931c80442c3558b196c3a976ef
amu1016 <amu.prog@gmail.com>
2021-12-12 06:51:40 +0900

    MutationObserver設定

--- app/javascript/clubs/checkbox.js
+++ app/javascript/clubs/checkbox.js
@@ -1,10 +1,3 @@
-import axios from 'axios';
-
-axios.defaults.headers.common = {
-  'X-Requested-With': 'XMLHttpRequest',
-  'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-};
-
 document.addEventListener('DOMContentLoaded', function(){
   var queryString = window.location.search;
   var queryObject = new Object();
@@ -18,34 +11,6 @@ document.addEventListener('DOMContentLoaded', function(){
       var paramValue = element[1];
       queryObject[paramName] = paramValue;
 
-      // 都道府県
-      // if (paramName === "q[prefecture_id_eq]") {
-      //   var elementId = paramName.replace("[", "_");
-      //   elementId = elementId.replace("]", "");
-      //   var selectTargets = document.getElementById(elementId);
-      //   selectTargets.options[paramValue - 1].selected = true;
-      //   // selectTargets.addEventListener('change', Sample.default.methods.getCities());
-      //   // axios
-      //   //   .post('/set_cities', {
-      //   //     id: paramValue
-      //   //   })
-      //   //   .then((response) => {
-      //   //     var cities = response.data
-      //   //   })
-      // };
-
-      // 市区町村
-      // if (paramName === "q[city_id_eq_any][]") {
-      //   var elementId = parameters[i].replace("[", "_");
-      //   elementId = elementId.replace("][]=", "_");
-      //   var checkTargets = document.getElementById(elementId);
-      //   for (var j = 0; j < checkTargets.length; j++){
-      //     if (checkTargets[j].value === paramValue) {
-      //       checkTargets[j].checked = true;
-      //       break;
-      //     };
-      //   };
-      // };
 
       // チェックボックス保持
       if (paramName === "q[status_id_eq_any][]") {
@@ -76,3 +41,36 @@ document.addEventListener('DOMContentLoaded', function(){
   };
 });
 
+
+var cityListsContent = getElementsByClassName('city-lists-content')
+var mo = new MutationObserver(function() {
+  var queryString = window.location.search;
+  var queryObject = new Object();
+  if(queryString){
+    queryString = queryString.substring(1);
+    var decodeParameters = decodeURIComponent(queryString);
+    var parameters = decodeParameters.split('&');
+    for (var i = 0; i < parameters.length; i++) {
+      var element = parameters[i].split('=');
+      var paramName = element[0];
+      var paramValue = element[1];
+      queryObject[paramName] = paramValue;
+
+
+      if (paramName === "q[city_id_eq_any][]") {
+        var cityList = document.getElementsByClassName('city-list')
+        for (var j = 0; j < cityList.length; j++){
+          var checkTargets = cityList[j].firstElementChild
+          if (checkTargets.value === `${paramValue}`) {
+            checkTargets.checked = true;
+            break;
+          };
+        };
+      };
+    };
+  };
+});
+var config = {
+  childList: true
+};
+mo.observe(cityListsContent, config);
