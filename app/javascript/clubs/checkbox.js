dabcfee128b276d1cbda048f226c49e977e7cc55
amu1016 <amu.prog@gmail.com>
2021-12-16 10:30:46 +0900

    js整理

--- app/javascript/clubs/checkbox.js
+++ app/javascript/clubs/checkbox.js
@@ -39,43 +39,6 @@ document.addEventListener('DOMContentLoaded', function(){
       };
     };
   };
-  var cityListsContent = document.getElementsByClassName('search-prefecture')[0];
-  if (cityListsContent){
-    var mo = new MutationObserver(function() {
-      alert('divの中身が変更されたよ');
-      var queryString = window.location.search;
-      var queryObject = new Object();
-      if(queryString){
-        queryString = queryString.substring(1);
-        var decodeParameters = decodeURIComponent(queryString);
-        var parameters = decodeParameters.split('&');
-        for (var i = 0; i < parameters.length; i++) {
-          var element = parameters[i].split('=');
-          var paramName = element[0];
-          var paramValue = element[1];
-          queryObject[paramName] = paramValue;
-    
-    
-          if (paramName === "q[city_id_eq_any][]") {
-            var cityList = document.getElementsByClassName('city-list')
-            for (var j = 0; j < cityList.length; j++){
-              var checkTargets = cityList[j].firstElementChild
-              if (checkTargets.value === `${paramValue}`) {
-                checkTargets.checked = true;
-                break;
-              };
-            };
-          };
-        };
-      };
-    });
-    var config = {
-      childList: true,
-      attributes: true,
-      characterData: true,
-    };
-    mo.observe(cityListsContent, config);
-  };
 });
 
 
